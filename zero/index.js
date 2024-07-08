f61b00d265450562ee9a33dd27c2080b57bd2207
Jan Pikl <pikl.jan@gmail.com>
2024-07-08 23:52:05 +0200

    Update zero

---
+++ zero/index.js
@@ -0,0 +1,116 @@
+const THEME = "dracula";
+const EDIT_TIMEOUT = 500;
+const EVAL_TIMEOUT = 500;
+
+const DEFAULT_INPUT = `
+// ========================================================
+//                         ZERO
+//              Minimalist JavaScript REPL
+//             https://github.com/jpikl/zero
+// ========================================================
+//
+// Built-in functions:
+//   print(...args)    print arguments
+//   println(...args)  print arguments + new line
+
+println('Hello world!');
+`;
+
+function main() {
+  const editor = ace.edit("editor");
+
+  editor.setTheme(`ace/theme/${THEME}`);
+  editor.setFontSize(16);
+  editor.setShowPrintMargin(false);
+
+  const session = editor.getSession();
+
+  session.setMode("ace/mode/javascript");
+  session.setTabSize(2);
+  session.setUseSoftTabs(true);
+
+  const output = document.getElementById("output");
+  output.classList.add(...editor.container.classList); // To match style with the editor
+
+  const evaluate = createEvaluator({
+    onSuccess: result => (output.innerHTML = result),
+    onError: error => output.innerHTML = `<div class="error">${error}</div>`,
+  });
+
+  editor.on("change", debounce(() => {
+    setHash(editor.getValue());
+    evaluate(editor.getValue());
+  }, EDIT_TIMEOUT));
+
+  editor.setValue(getHash() || DEFAULT_INPUT.trim());
+  editor.focus();
+}
+
+function createEvaluator({ onSuccess, onError }) {
+  let worker = null;
+
+  const handleMessage = event => {
+    const { id, error, output } = event.data;
+    clearTimeout(id);
+
+    if (error) {
+      onError(error);
+    } else {
+      onSuccess(output);
+    }
+  };
+
+  const handleTimeout = () => {
+    worker.terminate();
+    worker = null;
+    onError(`Evaluation exceeded ${EVAL_TIMEOUT}ms`);
+  };
+
+  return input => {
+    if (!worker) {
+      worker = new Worker("./worker.js");
+      worker.onmessage = handleMessage;
+    }
+
+    const id = setTimeout(handleTimeout, EVAL_TIMEOUT);
+    worker.postMessage({ id, input });
+  };
+}
+
+function getHash() {
+  const hash = location.hash.substring(1);
+  return hash ? binaryToText(atob(hash)) : null;
+}
+
+function setHash(value) {
+  const hash = value ? btoa(textToBinary(value)) : "";
+  history.replaceState(null, "", "#" + hash);
+}
+
+function textToBinary(text) {
+  const chars = new Uint16Array(text.length);
+  for (let i = 0; i < chars.length; i++) {
+    chars[i] = text.charCodeAt(i);
+  }
+  const bytes = new Uint8Array(chars.buffer);
+  return String.fromCharCode(...bytes);
+}
+
+function binaryToText(binary) {
+  const bytes = new Uint8Array(binary.length);
+  for (let i = 0; i < bytes.length; i++) {
+    bytes[i] = binary.charCodeAt(i);
+  }
+  const chars = new Uint16Array(bytes.buffer);
+  return String.fromCharCode(...chars);
+}
+
+function debounce(callback, delay) {
+  let id = 0;
+  return (...args) => {
+    clearTimeout(id);
+    id = setTimeout(() => callback(...args), delay);
+  };
+}
+
+main();
---
+++ zero/worker.js
@@ -0,0 +1,48 @@
+// All internal variables here are prefixed with `__` in order to obfuscate
+// them  for the `eval` call which could potentially overwrite them.
+
+const __MAX_CHAR_COUNT = 1024 * 1024;
+const __output = [];
+let __charCount = 0;
+
+// Built-in function available to be called from `eval`.
+const print = (...args) => {
+  for (const arg of args) {
+    const strArg = typeof arg !== "string" ? JSON.stringify(arg) : arg;
+    __output.push(strArg);
+    __charCount += strArg.length;
+
+    if (__charCount > __MAX_CHAR_COUNT) {
+      throw new Error(`Output exceeded ${__MAX_CHAR_COUNT} characters`);
+    }
+  }
+};
+
+// Built-in function available to be called from `eval`.
+const println = (...args) => print(...args, "\n");
+
+const __postMessage = postMessage;
+postMessage = undefined;
+
+const __onmessage = __event => {
+  __output.length = 0;
+  __charCount = 0;
+
+  try {
+    eval(__event.data.input);
+
+    __postMessage({
+      id: __event.data.id,
+      output: __output.join(""),
+    });
+  } catch (error) {
+    __postMessage({
+      id: __event.data.id,
+      error: error.toString(),
+    });
+  } finally {
+    onmessage = __onmessage;
+  }
+};
+
+onmessage = __onmessage;
